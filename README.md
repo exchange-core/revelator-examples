9ae6bb2ce23c6ada697b7c15015894922cc950ba
Maksim Zheravin <mzheravin@core2.exchange>
2022-09-11 14:07:22 -0400

    Add examples - payments

---
+++ README.md
@@ -0,0 +1,20 @@
+# Revelator examples
+
+[![][license img]][license]
+
+Revelator is a **low latency messages processing** framework in Java 17.
+
+This project contains examples for different usages of the framework.
+
+### Payments
+
+Accounting system built on top of Revelator framework.
+It demonstrates a microservice for high-speed money transfer between different accounts, similar to PayPal.
+
+### Contributing
+
+Revelator is an open-source project and contributions are welcome!
+
+[license]:LICENSE
+
+[license img]:https://img.shields.io/badge/License-Apache%202-blue.svg
---
+++ src/main/java/exchange/core2/revelator/examples/payments/TransferSession.java
@@ -0,0 +1,41 @@
+package exchange.core2.revelator.examples.payments;
+
+import exchange.core2.revelator.processors.pipelined.PipelinedFlowSession;
+
+public final class TransferSession extends PipelinedFlowSession {
+
+    public long accountSrc;
+    public long accountDst;
+
+    public boolean processSrc;
+    public boolean processDst;
+
+    public long amountSrc;
+    public long amountDst;
+
+    public long treasureAmountSrc;
+    public long treasureAmountDst;
+
+    public boolean localPartSucceeded;
+
+
+    //    public short currency;
+
+
+    @Override
+    public String toString() {
+        return "TransferSession{" +
+                "accountSrc=" + accountSrc +
+                ", accountDst=" + accountDst +
+                ", processSrc=" + processSrc +
+                ", processDst=" + processDst +
+                ", globalOffset=" + globalOffset +
+                ", bufferIndex=" + bufferIndex +
+                ", payloadSize=" + payloadSize +
+                ", messageType=" + messageType +
+                ", timestamp=" + timestamp +
+                ", correlationId=" + correlationId +
+                ", userCookie=" + userCookie +
+                '}';
+    }
+}
